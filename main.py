599d3f301c05d9ecab285edadb381ed5cbfb7ee0
Sime Martinov <sime.martinov@rathmann.hr>
2019-01-29 13:38:07 +0100

    fafa

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 app.debug = True
@@ -14,15 +15,29 @@ def parkovi():
 
 @app.route("/plitvice")
 def plitvice():
-    return render_template("plitvice.html")
+
+    return render_template("plitvice.html", jsonWeather=getWeather(3188691))
 
 @app.route("/risnjak")
 def risnjak():
-    return render_template("risnjak.html")
+    return render_template("risnjak.html", jsonWeather=getWeather(3202104))
 
 @app.route("/paklenica")
 def paklenica():
-    return render_template("paklenica.html")
+    return render_template("paklenica.html", jsonWeather=getWeather(3189964))
+
+
+def getWeather(id):
+
+    parameters = { 'appid': '7f1839b423ed0ec9c2c366cab3867ca2',
+    'id': id, 'units': 'metric', 'lang': 'hr' }
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    
+    response = requests.get(url, params=parameters)
+    
+    jsonWeather = response.json()
+    
+    return jsonWeather
 
 if __name__ == "__main__":
     app.run()
